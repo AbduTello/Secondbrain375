d03529b36df1d8412854991e68137cfe9f15323d
Abdu_Tello <abdulta@umich.edu>
2024-11-28 16:40:55 -0500

    changed title in react app

--- secondBrain/app/(tabs)/index.tsx
+++ secondBrain/app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Secondbrain</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
