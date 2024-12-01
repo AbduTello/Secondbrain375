f7b44db986763167bb05377aa13d82d2e8579cbc
Abdu_Tello <abdulta@umich.edu>
2024-12-01 02:34:39 -0500

    put icons in nav bar

--- secondBrain/app/(tabs)/_layout.tsx
+++ secondBrain/app/(tabs)/_layout.tsx
@@ -1,16 +1,32 @@
 import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function TabsLayout() {
   return (
-  <Tabs>
+  <Tabs
+    screenOptions={{
+        tabBarActiveTintColor: "#006EE9",
+    }}
+  >
     <Tabs.Screen name='index' options={{
       headerTitle: " Tasks",
+      tabBarIcon: ({focused, color}) => <Ionicons name={focused ? "checkmark-circle-sharp" : "checkmark-circle-outline"}
+      color={"#006EE9"}
+      size={25} />
     }} />
+
     <Tabs.Screen name='addTask' options={{
       headerTitle: " Add Task",
+      tabBarIcon: ({focused, color}) => <Ionicons name={focused ? "add-circle-sharp" : "add-circle-outline"}
+      color={"#006EE9"}
+      size={25} />
+
     }} />
     <Tabs.Screen name='calendar' options={{
       headerTitle: " Calendar",
+      tabBarIcon: ({focused, color}) => <Ionicons name={focused ? "calendar-sharp" : "calendar-outline"}
+      color={"#006EE9"}
+      size={25} />
     }} />
   </Tabs>
 );
